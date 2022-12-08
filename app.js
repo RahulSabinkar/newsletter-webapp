f5ffe04d6485f118d3913801c6bcb6595d4ab066
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2022-12-09 00:11:53 +0530

    replaced node:https with @mailchimp
    Instead of using https.request to send POST request data to mailchimp
    endpoint, added official @mailchimp node module and utilized it's
    functionality.

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require("dotenv").config({ path: __dirname + "/.env" });
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("node:https");
+const client = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Create your own .env file at the root of the project directory and
-// enter your own API keys, username, dc and list-id.
-const apiKey = process.env.API_USERNAME + ":" + process.env.API_KEY;
-const dc = process.env.DC;
+// enter your own API key, dc and list-id.
+client.setConfig({
+  apiKey: process.env.API_KEY,
+  server: process.env.DC,
+});
 const listID = process.env.LIST_ID;
 
 app.use(express.static("public"));
@@ -42,29 +44,17 @@ app.post("/", (req, res) => {
     ],
   };
 
-  const jsonData = JSON.stringify(data);
-
-  const url = `https://${dc}.api.mailchimp.com/3.0/lists/${listID}`;
-
-  const options = {
-    method: "POST",
-    auth: apiKey,
+  const run = async () => {
+    const response = await client.lists.batchListMembers(listID, data);
+    console.log(response);
+    if (!response.error_count) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      // TODO: Add error, error-code dynamically to failure.html
+      res.sendFile(__dirname + "/failure.html");
+    }
   };
-
-  const request = https.request(url, options, (response) => {
-    response.on("data", (data) => {
-      const parsedData = JSON.parse(data);
-      console.log(parsedData);
-      if (response.statusCode === 200) {
-        res.sendFile(__dirname + "/success.html");
-      } else {
-        res.sendFile(__dirname + "/failure.html");
-      }
-    });
-  });
-
-  request.write(jsonData);
-  request.end();
+  run();
 });
 
 app.listen(port, () => {
